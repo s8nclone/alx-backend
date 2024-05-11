e78b0b35fa247d5c5e14f94e76f564500708a944
s8nclone <dayojnr.3@gmail.com>
2024-05-11 09:22:52 +0000

    deletion-resilient hypermedia pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-Implement a get_hyper_index method with two args indexwith a none default value
-and page_size with a defualt value of 10 and returns a dictionary with key-value pairs
+Implement a get_hyper_index method with two args index with a None default
+value and page_size with a defualt value of 10 and returns a dictionary 
+with key-value pairs
 """
 import csv
 from typing import Dict, List
